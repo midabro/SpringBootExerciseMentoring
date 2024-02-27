09f094b3796789de235069c1e250dbb2f5b5f076
Michal Dabrowski <midabro@gmail.com>
2024-02-27 15:17:44 +0100

    WIP: Creation of an additional module for the main project class

---
+++ src/main/java/com/sii/springbootexercisementoring/package-info.java
@@ -0,0 +1,6 @@
+@org.springframework.modulith.ApplicationModule(
+    allowedDependencies = "address/com/sii/springbootexercisementoring"
+
+)
+
+package com.sii.springbootexercisementoring;
---
+++ src/test/java/SpringModulithTests.java
@@ -0,0 +1,6 @@
+//import org.springframework.modulith.core.ApplicationModules;
+
+public class SpringModulithTests {
+//  ApplicationModules modules;
+
+}
